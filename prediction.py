ce25c38bbae6d8c15ae08c376acdcc5962af2182
atiqamemonadeem <atiqamemonadeem@gmail.com>
2026-02-13 23:34:36 -0800

    Update prediction.py
    AI model which take 5 inputs(symptoms from user)

--- prediction.py
+++ prediction.py
@@ -1,24 +1,32 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
 import pickle
 
 
 data = pd.read_csv("disease_prediction_dataset.csv")
 
 
-X = data.drop("disease", axis=1)   # symptoms
-y = data["disease"]                # target disease
+selected_features = ["fever", "cough", "fatigue", "headache", "nausea"]
+
+X = data[selected_features]
+y = data["disease"]
+
+
+le = LabelEncoder()
+y_encoded = le.fit_transform(y)
 
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y_encoded, test_size=0.2, random_state=42
 )
 
 
-model = DecisionTreeClassifier()
+model = RandomForestClassifier(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
+
 
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
@@ -28,4 +36,7 @@ print("Model Accuracy:", accuracy)
 with open("disease_model.pkl", "wb") as f:
     pickle.dump(model, f)
 
-print("✅ Model trained and saved successfully")
+with open("label_encoder.pkl", "wb") as f:
+    pickle.dump(le, f)
+
+print("Model and encoder trained and saved successfully")
