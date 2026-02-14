b2722a38bfc5b43830efba8acd863f3cddb08d74
Hadia Jawed <108028738+RaniaAbfhm@users.noreply.github.com>
2026-02-14 12:22:50 +0500

    Delete Main.java created another main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        medicine med= new medicine("Paracetamol","8:00");
-        med.takemedinfo();
-        Notification reminder=new Notification();
-        reminder.notify(med);
-    }
-}
