974855bd1c852c3999517946528410a696d13f43
Hadia Jawed <108028738+RaniaAbfhm@users.noreply.github.com>
2026-02-14 11:08:08 +0500

    fixed an error

--- Notification.java
+++ Notification.java
@@ -4,7 +4,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class MedicineReminder {
+public class Notification {
     static boolean triggered = false;
     
     public static void main(String[] args) {
@@ -79,3 +79,4 @@ public class MedicineReminder {
         }
     }
 }
+
